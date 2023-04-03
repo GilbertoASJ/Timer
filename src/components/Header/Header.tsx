bde5aacb95177844916a01919d5e35a2e325663d
Gilberto Alves De Sousa Júnior <gilberto.sjunior@valid.com>
2023-04-03 12:57:26 -0300

    Feature: :sparkles: header & layout

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,21 @@
+import { NavLink } from "react-router-dom";
+import { HeaderContainer } from "./styles";
+import { HourglassHigh, Timer, Scroll } from 'phosphor-react';
+
 export function Header() {
     return (
-        <>
-            <h1>Header</h1>
-        </>
+        <HeaderContainer>
+            <span>
+                <HourglassHigh size={32} />
+            </span>
+            <nav>
+                <NavLink to="/" title="Timer">
+                    <Timer size={24} />
+                </NavLink>
+                <NavLink to="/historico" title="Histórico">
+                    <Scroll size={24} />
+                </NavLink>
+            </nav>
+        </HeaderContainer>
     )
 }
