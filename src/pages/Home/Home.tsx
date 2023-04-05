8dc1995f8b44bcef20fc238571f132779a9d09eb
Gilberto Alves De Sousa Júnior <gilberto.sjunior@valid.com>
2023-04-05 13:26:30 -0300

    Feature: :sparkles: new cycle

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 // Imports
+import { useState } from 'react';
 import { Play } from 'phosphor-react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
@@ -27,9 +28,20 @@ const newCycleFormValidationSchema = zod.object({
 // Tipagens
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
+interface Cycle {
+    id: string;
+    task: string;
+    minutesAmount: number;
+}
+
 export function Home() {
 
-    const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+    // States
+    const [cycles, setCycles] = useState<Cycle[]>([]);
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
+    // React hook form
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
@@ -39,8 +51,25 @@ export function Home() {
 
     function handleCreateNewCycle(data: NewCycleFormData) {
 
+        const id = String(new Date().getDate());
+
+        const newCycle: Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount
+        }
+
+        setCycles((state) => [...state, newCycle]);
+        setActiveCycleId(id);
+
+        reset();
         console.log(data);
     }
+
+    // Encontrar qual ciclo está ativo
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+    console.log(activeCycle);
 
     const task = watch('task');
     const isSubmitDisabled = !task;
